91d6f79c9f0bf8f9d59f51a51b176b89e82f0659
MsLJ <134290720+MsLJ@users.noreply.github.com>
2023-11-05 20:18:30 +0900

    Update p01_basic.py

--- Jul27_3_Image/p01_basic.py
+++ Jul27_3_Image/p01_basic.py
@@ -1,25 +1,33 @@
 # -*- coding:utf-8 -*-
 
-#pip install --upgrade pip setuptools wheel
-#pip install opencv-python
-#pip install opencv-python==3.4.0.12
+# visual studio 2022설치하고
+# pip install --upgrade pip setuptools wheel
+# pip install opencv-python==3.4.0.12
 import cv2
-
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
-fontFile="C:/Windows/Fonts/malgun.ttf"
-fontName=fm.FontProperties(fname=fontFile,size=10).get_name()
-plt.rc("font",family=fontName)
 
-a=cv2.imread("C:/lee/z.png",cv2.IMREAD_GRAYSCALE)
+FontFile = "C:/Windows/Fonts/malgun.ttf"
+fontName = fm.FontProperties(fname=FontFile, size=10).get_name()
+plt.rc("font", family=fontName)
+
+# ML/DL
+# 이미지/사운드/영상 : 컴에는 10101로 저장
+#    그 데이터를 인코딩만 할 줄 알면
+
+# 픽셀이 아예 흰색 : 255
+# 픽셀이 아예 검정 : 0
+# 픽셀 하나를 0 ~ 255로
+a = cv2.imread("C:/Users/Default/Pictures/test.png", cv2.IMREAD_GRAYSCALE)
 print(a)
+print("--------")
 
-#픽셀 하나를 [255 0 0]
-b=cv2.imread("C:/lee/z.png",cv2.IMREAD_GRAYSCALE)
-print(b)
-#BGR->RGB로
-b=cv2.cvtColor(b,cv2.COLOR_BGR2RGB)
+# 픽셀 하나를 [B G R]로
+
+b = cv2.imread("C:/Users/Default/Pictures/test.png", cv2.IMREAD_COLOR)
+# BGR -> RGB로
+b = cv2.cvtColor(b, cv2.COLOR_BGR2RGB)
 print(b)
 
-plt.imshow(a)
+plt.imshow(b)
 plt.show()
